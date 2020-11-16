50218436ce30703f3683d73886f09f2a220c98d3
hnvnam19it3 <54528683+hnvnam19it3@users.noreply.github.com>
2020-11-16 09:27:02 +0700

    Larvel demo with ver 8.0 news page

---
+++ app/Models/LoaiTin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LoaiTin extends Model
+{
+    use HasFactory;
+    protected $table = "LoaiTin";
+
+    public function theloai()
+    {
+        return $this->belongsTo('App\TheLoai', 'idTheLoai', 'id');
+    }
+
+    public function tintuc()
+    {
+        return $this->hasMany('App\Tintuc', 'idLoaiTin', 'id');
+    }
+}
