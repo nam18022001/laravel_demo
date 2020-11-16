107f3a20e2e51c0a1d605e16641f38ae2f24e87f
hnvnam19it3 <54528683+hnvnam19it3@users.noreply.github.com>
2020-11-16 10:55:33 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('thu', function () {
+    return view('admin.theloai.danhsach');
+});
